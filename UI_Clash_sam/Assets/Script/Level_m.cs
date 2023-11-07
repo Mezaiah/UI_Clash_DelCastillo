a1bdd3126ea107a02dda333d129c523125f064ee
Mezaiah <samueldelcastillo1710@gmail.com>
2023-11-07 03:01:06 -0600

    update _ examen

--- UI_Clash_sam/Assets/Script/Level_m.cs
+++ UI_Clash_sam/Assets/Script/Level_m.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 using System.IO;
 using SimpleJSON;
 using TMPro;
+using UnityEditor.Experimental.GraphView;
 
 public class Level_M : MonoBehaviour
 {
@@ -38,7 +39,37 @@ public class Level_M : MonoBehaviour
             {
                 characters[i].transform.GetChild(1).GetComponent<TMP_Text>().text = characterz[i].name;
                 characters[i].transform.GetChild(3).GetChild(0).GetComponent<TMP_Text>().text = characterz[i].id.ToString();
+           if (characterz[i].name == "Golemite" ||
+                    characterz[i].name == "TowerPrincess" || 
+                    characterz[i].name == "LavaPups" || 
+                    characterz[i].name == "RageBarbarian" ||
+                    characterz[i].name == "AngryBarbarian"||
+                    characterz[i].name == "Assassin"||
+                    characterz[i].name == "GhostOverlay" ||
+                    characterz[i].name == "MiniZapMachine" ||
+                    characterz[i].name == "BrokenCannon" ||
+                    characterz[i].name == "RascalGirl" ||
+                    characterz[i].name == "DartBarrell" ||
+                    characterz[i].name == "NOTINUSECH" ||
+                    characterz[i].name == "NOTINUSEEG" ||
+                    characterz[i].name == "NOTINUSECH" ||
+                    characterz[i].name == "SpearGoblinGiant" ||
+                    characterz[i].name == "REPLACEW" ||
+                    characterz[i].name == "REPLACEWA" ||
+                    characterz[i].name == "NOTINUSEAM" ||
+                    characterz[i].name == "REPLACEED" ||
+                    characterz[i].name == "REPLACEWB" )
+                {
+                characters[i].transform.GetChild(0).GetComponent<Image>().sprite = Resources.Load<Sprite>("Cardzz/" + "no");
+                   
+                }
+                else
+                {
                 characters[i].transform.GetChild(0).GetComponent<Image>().sprite = Resources.Load<Sprite>("Cardzz/" + characterz[i].name);
+
+                }
+           
+            
             }
         }
     }
---
+++ UI_Clash_sam/Assets/Script/slide_an1.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class slide_an1 : MonoBehaviour
+{
+    public GameObject container;
+    
+    public void ShowHide()
+    {
+        if ( container != null)
+        {
+            Animator animator = container.GetComponent<Animator>();
+            if (animator != null)
+            {
+                bool IsClicked = animator.GetBool("Moves");
+                animator.SetBool("Moves", true);
+            }
+        }
+    }
+}
